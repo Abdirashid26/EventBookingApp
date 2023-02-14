6cf64d42567467b5145c21a358c0e80ad0aba5aa
faisal <anzifai6387@gmail.com>
2023-02-14 22:44:39 +0300

    added the dagger-hilt library dependency

---
+++ app/src/main/java/com/example/eventbookingapp/onboarding/MainOnboardingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.eventbookingapp.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eventbookingapp.R
+
+
+class MainOnboardingFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_onboarding, container, false)
+    }
+
+}
